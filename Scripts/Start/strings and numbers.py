d77a77ff3378aa8aacf9eff4ed864915d70d3bec
PabloRamirez <PabloRamirez@par.innovations.com>
2023-06-12 12:08:46 +0200

    strings and numbers

---
+++ Scripts/Start/strings and numbers.py
@@ -0,0 +1,23 @@
+print("Hola Mundo")
+print ("asdf")
+print ("hhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
+
+numero_dos = 2
+numero_cuatro = 4
+
+print(numero_dos)
+print(numero_cuatro)
+print(numero_dos + numero_cuatro)
+
+#asignacion
+
+
+texto = "Esto es un texto"
+texto += " "
+texto += "a que si pablo?"
+print(texto)
+
+numero1 = 23.3
+numero2 = 23424.9
+numero3 = 66.9
+print(numero1+ numero2 + numero3)
