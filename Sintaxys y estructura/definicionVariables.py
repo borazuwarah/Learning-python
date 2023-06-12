e8798a49ccbae0ed5ee104116f1323d3be76cf88
PabloRamirez <PabloRamirez@par.innovations.com>
2023-06-12 13:36:28 +0200

    a

---
+++ Sintaxys y estructura/definicionVariables.py
@@ -0,0 +1,8 @@
+nombre = "Juan"
+edad = 25
+precio = 9.99
+
+
+"""
+En Python, no es necesario declarar explícitamente el tipo de variable. Puedes asignar un valor directamente y Python inferirá el tipo de dato.
+"""
