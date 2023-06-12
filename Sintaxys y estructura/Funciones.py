c416c20e3996f131da414727f8985a000c8cdea7
PabloRamirez <PabloRamirez@par.innovations.com>
2023-06-12 13:35:45 +0200

    funciones instrucciones y definicion de variables

---
+++ Sintaxys y estructura/Funciones.py
@@ -0,0 +1,13 @@
+def suma(a, b):
+    return a + b
+
+resultado = suma(3, 5)
+print(resultado)  # Imprime: 8
+
+"""
+Este ejemplo muestra cómo definir una función llamada suma que toma dos parámetros (a y b) y retorna la suma de ellos. Luego, se llama a la función con los argumentos 3 y 5, y el resultado se asigna a la variable resultado.
+
+Recuerda que la indentación (espacios en blanco al comienzo de una línea) es crucial en Python, ya que se utiliza para indicar la estructura y los bloques de código. Por lo tanto, es importante asegurarse de que los bloques de código tengan la misma indentación para que Python los interprete correctamente.
+
+Estos ejemplos son solo una introducción básica a la sintaxis y estructura de Python. Hay muchas más características y elementos en el lenguaje que puedes explorar, como bucles (for, while), listas, diccionarios, entre otros.
+"""
---
+++ Sintaxys y estructura/InstruccionesYBloques.py
@@ -0,0 +1,11 @@
+if edad >= 18:
+    print("Eres mayor de edad")
+    print("Puedes ingresar al bar")
+else:
+    print("Eres menor de edad")
+    print("No puedes ingresar al bar")
+
+
+"""
+En este ejemplo, se utiliza la instrucción if-else para realizar una verificación condicional. El bloque de código indentado después de la declaración if se ejecutará si la condición es verdadera. Si la condición es falsa, se ejecutará el bloque indentado después de la declaración else.
+"""
